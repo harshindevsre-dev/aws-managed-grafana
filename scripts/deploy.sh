8ec4213aa436213b3069ddb9dac75363fbdbf328
harshindevsre-dev <harshindev.sre@gmail.com>
2026-05-12 15:38:13 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "🚀 Deploying Grafana Dashboards..."
+echo " Deploying Grafana Dashboards..."
 
 for file in dashboards/*.json
 do
   echo "Uploading $file"
 
   curl -X POST "$GRAFANA_URL/api/dashboards/db" \
-    -H "Authorization: Bearer $GRAFANA_API_KEY" \
-    -H "Content-Type: application/json" \
-    -d @"$file"
+  -H "Authorization: Bearer $GRAFANA_API_KEY" \
+  -H "Content-Type: application/json" \
+  -d @grafana-alert-policy.json
 
 done
 
-echo "✅ Dashboards deployed successfully"
+echo " Dashboards deployed successfully"
